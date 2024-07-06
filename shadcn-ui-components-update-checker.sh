5a10b0e14d1bfaca67bd0ceb6acf094f635442a7
shinichi-takii <shinichi.takii@shaketh.com>
2024-07-06 23:12:27 +0900

    Add path parameter / Enhance README

--- shadcn-ui-components-update-checker.sh
+++ shadcn-ui-components-update-checker.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+set -e
+
+# Check for directory parameter
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <react_project_directory_path>"
+    exit 1
+fi
+
+# Change to the specified directory
+echo "$1"
+cd "$1" || { echo "Cannot change to specified directory: $1"; exit 1; }
+
+
 # Run 'npx shadcn-ui@latest diff' and get the result
 diff_output=$(npx shadcn-ui@latest diff)
 
@@ -19,3 +32,7 @@ for component in $components; do
   echo -e "- Diff: 'npx shadcn-ui@latest diff \e[1;33m$component\e[0m'"
   echo -e "- Update: 'npx shadcn-ui@latest add \e[1;33m$component\e[0m'"
 done
+
+
+# Return to the original directory
+cd - > /dev/null
